6315a3513ee47bfb157e3a9c497bb87c2cd67e1a
fr0tt <33751346+fr0tt@users.noreply.github.com>
2018-07-26 23:27:35 +0200

    yaml content instead of json & try to implement a category feature

--- build.js
+++ build.js
@@ -1,47 +1,69 @@
-const fse = require('fs-extra')
+const fs = require('fs-extra')
 const path = require('path')
+const yaml = require('js-yaml')
 const { promisify } = require('util')
 const nunjucks = require('nunjucks')
 const nunjucksRender = promisify(nunjucks.render)
 const glob = promisify(require('glob'))
-const config = require('./site.config')
 
+const config = require('./site.config')
 const distPath = config.build.distPath 
+const defaultLayout = config.build.defaultLayout
+const layouts = config.layouts
 
 // copy assets folder
-fse.copy('static', `${distPath}/static`)  //fse.copy(srcPath + '/assets', `${distPath}/assets`) 
-  .catch(err => {
-    console.error(err)     //https://github.com/jprichardson/node-fs-extra/blob/master/docs/copy.md
-  })
+fs.copy('static', `${distPath}/static`)  //fs.copy(srcPath + '/assets', `${distPath}/assets`) 
+  .catch(err => { console.error(err) })
 
 // read page templates
 // looking for .json files, return paths of found files
 // cwd: The current working directory in which to search
 
-glob('**/*.json', { cwd: 'content' })
+let categories = []
+
+glob('**/*.yaml', { cwd: 'content' })
+  .then((files) => {
+    files.forEach((file) => {
+      let data = yaml.safeLoad(fs.readFileSync(`content/${file}`))
+      console.log(data.category)
+      categories.push(data.category)
+    })
+  })
+  .catch((err) => { console.error(err) })
+
+glob('**/*.yaml', { cwd: 'content' })
   .then((files) => {
     files.forEach((file) => {
       // get info data
       const fileInfo = path.parse(file)
 
-      // combine distPath and dir of a found .njk file 
-      const destPath = path.join(distPath, fileInfo.dir)
-      console.log(destPath)
+      // destination path without filename and extension 
+      const destDir = path.join(distPath, fileInfo.dir)
 
       // defines a current working directory in which to search
       nunjucks.configure('layouts')
       // create destination directory
-      fse.mkdirs(destPath)
-        .then((pageContents) => {
+      fs.mkdirs(destDir)
+        .then(() => {
           // render layout with page contents
-          console.log(pageContents)
-          return nunjucksRender('base.njk', require(`./content/${file}`))
+          let layout = `${defaultLayout}.njk`
+          layouts.forEach((rule) => {
+            if (`content/${file}`.includes(rule.path)) {
+              layout = rule.layout
+            }
+          })
+          return nunjucksRender(layout, yaml.safeLoad(fs.readFileSync(`content/${file}`))) //require(`./content/${file}`)
         })
-        .then((layoutContent) => {
+        .then((pageContent) => {
           // save the html file
-          fse.writeFile(`${destPath}/${fileInfo.name}.html`, layoutContent)
+          fs.writeFile(`${destDir}/${fileInfo.name}.html`, pageContent)
         })
         .catch((err) => { console.error(err) })
     })
   })
+  .then(() => {
+    // execute function after building
+    config.postHook()
+  })
   .catch((err) => { console.error(err) })
+  
