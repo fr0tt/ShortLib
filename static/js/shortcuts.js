027f6ff84c1a34d1ab9016115a131f1cd1c362d7
fr0tt <33751346+fr0tt@users.noreply.github.com>
2018-08-11 00:40:05 +0200

    js for app layout

---
+++ static/js/shortcuts.js
@@ -0,0 +1,46 @@
+const winIndex = OS.indexOf('win') 
+const macIndex = OS.indexOf('mac')
+const linuxIndex = OS.indexOf('linux')
+
+let vue = new Vue({
+    el: '.contentside',
+    data: {
+        filterQuery: '',
+        filterIsActive: false,
+        selectedOS: '',
+        showOS: false
+    },
+    methods: {
+        containsFilter: function (value) {
+            return value.toLowerCase().indexOf(this.filterQuery.toLowerCase()) !== -1
+        },
+        highlight: function (value) {
+            return value.replace(new RegExp(this.filterQuery, 'gi'), '<mark>$&</mark>')
+        },
+        showOSAsAnOption: function (os) {
+            if (this.selectedOS === os) {
+                return false
+            }
+            if (OS.indexOf(os) === -1) {
+                return false
+            }
+            return true
+        }
+    }
+})
+
+let userOS = 'win'
+if (navigator.platform.toLowerCase().indexOf('mac') !== -1) {
+    userOS = 'mac'
+} else if (navigator.platform.toLowerCase().indexOf('linux') !== -1) {
+    userOS = 'linux'
+}
+if (OS.indexOf(userOS) !== -1) {
+    vue.selectedOS = userOS
+} else {
+    if (OS.indexOf('win') !== -1) {
+        vue.selectedOS = 'win'
+    } else {
+        vue.selectedOS = OS[0]
+    }
+}
