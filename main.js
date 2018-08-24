c98b59a9fe357846e3b20c587688764ecb6a5218
fr0tt <33751346+fr0tt@users.noreply.github.com>
2018-08-24 22:00:25 +0200

    add a sitemap

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const path = require('path')
 const yaml = require('js-yaml')
 const { promisify } = require('util')
 const glob = promisify(require('glob'))
+const sm = require('sitemap')
 
 const nunjucks = require('nunjucks')
 const nunjucksRender = promisify(nunjucks.render)
 
 const config = require('./site.config')
 const distPath = config.build.distPath 
-const defaultLayout = config.build.defaultLayout
 const layouts = config.layouts
 
 module.exports = {
@@ -46,16 +46,18 @@ module.exports = {
 
     // read page templates
     // looking for .yaml files, return paths of found files
-    // cwd: The current working directory in which to search
+    // cwd: The current working directory in which to search 
 
     glob('**', { cwd: 'content' }) //glob('**/*.yaml', { cwd: 'content' })
         .then((files) => {
             config.preHook(files)
             return files
         })
-        .then((files) => { 
+        .then((files) => {
+
+            let urls = []
             files.forEach((file) => {
-                
+
                 if (file.endsWith('.yaml')) {
 
                     // get info data
@@ -64,42 +66,53 @@ module.exports = {
                     const destDir = path.join(distPath, fileInfo.dir)
                     // defines a current working directory in which to search
                     // create destination directory
-                    fs.mkdirs(destDir)
-                    .then(() => {
-                        // render layout with page content
-                        let layout = `${defaultLayout}.njk`
-                        let data = yaml.safeLoad(fs.readFileSync(`content/${file}`))
-                        data.Page = {}
-                        // allow layouts to access absolute path
-                        data.Page.path = fileInfo
-                        // check if a costum layout file was specified (with a rule)
-                        for (let rule of layouts) {
-                            if (`content/${file}`.includes(rule.path)) {
-                                layout = rule.layout
-                                let ruleData = rule.data
-                                if (ruleData) {
+                    fs.mkdirsSync(destDir)
+                    // render layout with page content
+                    let layout
+                    let data = yaml.safeLoad(fs.readFileSync(`content/${file}`))
+                    data.Page = {}
+                    // allow layouts to access absolute path
+                    data.Page.path = fileInfo
+                    // check if a costum layout file was specified (with a rule)
+                    for (let rule of layouts) {
+                        if (`content/${file}`.includes(rule.path)) {
+                            layout = rule.layout
+                            let url = {
+                                url: rule.path,
+                                changefreq: 'weekly',
+                                priority: rule.priority,
+                                lastmodrealtime: true,
+                                lastmodfile: `content/${file}`
+                            }
+                            urls.push(url)
+                            let ruleData = rule.data
+                            if (ruleData) {
                                 if (ruleData.indexOf('static/') === -1)
                                     ruleData = `static/${ruleData}`
                                 if (ruleData.indexOf('.yaml') === -1)
                                     ruleData = `${ruleData}.yaml`
                                 data.Data = (yaml.safeLoad(fs.readFileSync(`${distPath}/${ruleData}`)))
-                                }
-                                break
                             }
+                            break
                         }
-                        return nunjucksRender(layout, data)
-                    })
-                    .then((pageContent) => {
-                        // save the html file
-                        fs.writeFile(`${destDir}/${fileInfo.name}.html`, pageContent)
-                    })
-                    .catch((err) => { console.error(err) })
-
+                    } 
+                    if (layout) {
+                        fs.writeFile(`${destDir}/${fileInfo.name}.html`, nunjucksRender(layout, data))
+                            .catch((err) => { console.error(err) })
+                    }
                 } else if (file.indexOf('.') !== -1) {
                     fs.copy(`content/${file}`, `${distPath}/${file}`)
                         .catch(err => { console.error(err) })
                 }
+                
             })
+            let sitemap = sm.createSitemap({
+                hostname: 'https://www.shortlib.com',
+                cacheTime: 600000,  //600 sec (10 min) cache purge period
+                urls: urls
+            })
+            fs.writeFile(`${distPath}/sitemap.xml`, sitemap.toString())
+                .catch((err) => { console.error(err) })
         })
         .catch((err) => { console.error(err) })
 
--- site.config.js
+++ site.config.js
@@ -9,10 +9,10 @@ module.exports = {
     distPath: 'public'
   },
   layouts: [
-    { path: '/apps/', layout: 'app.njk' },
-    { path: '/index', layout: 'index.njk', data: 'data.yaml' },
-    { path: '/contact', layout: 'contact.njk' },
-    { path: '/legal_notice', layout: 'legal_notice.njk' }
+    { path: '/apps/', layout: 'app.njk', priority: 0.80 },
+    { path: '/index', layout: 'index.njk', data: 'data.yaml', priority: 1.00 },
+    { path: '/contact', layout: 'contact.njk', priority: 0.20 },
+    { path: '/legal_notice', layout: 'legal_notice.njk', priority: 0.20 }
   ],
   preHook: ((files) => {
     util.generateData('public', files)
